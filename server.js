80b8418b3e67a5ff29eb40781dd029eacd89dfae
Michelle Cheng <cheng.michelley@gmail.com>
2026-02-01 22:34:04 -0500

    Exercise 2

--- server.js
+++ server.js
@@ -9,3 +9,13 @@ app.get('/greeting/:username', (req,res) => {
 })
 
 app.listen(3000, () => console.log('the server is running on port 3000'))
+
+app.get('/roll/:number', (req, res) => {
+    const number = parseInt(req.params.number)
+    const roll = Math.floor(Math.random() * number) + 1;
+    if(number && number > 0){
+    res.send(`<h1>You rolled a ${roll}!</h1>`)
+}else{
+    res.send(`<h1>Please provide a valid number!</h1>`)
+}
+})
