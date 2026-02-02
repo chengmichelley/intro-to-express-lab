716bed1381e91e4788554a79964cbe926fcf1ffb
Michelle Cheng <cheng.michelley@gmail.com>
2026-02-01 22:24:05 -0500

    Exercise 1

--- server.js
+++ server.js
@@ -2,4 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.get('/greeting/:username', (req,res) => {
+    const username = req.params.username
+    const capitalizedUsername = username.charAt(0).toUpperCase() + username.slice(1)
+    res.send(`<h1>Hello there, ${capitalizedUsername}!</h1>`)
+})
+
 app.listen(3000, () => console.log('the server is running on port 3000'))
